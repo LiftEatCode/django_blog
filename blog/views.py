43f69ddc239381d1949a3c52e43d7c59a52b9c23
LiftEatCode <joshuaspradlingtx@gmail.com>
2020-03-21 21:03:55 -0500

    edit post enabled

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView # new
 
-from django.views.generic.edit import CreateView # new
+from django.views.generic.edit import CreateView, UpdateView # new
 
 from .models import Post
 
@@ -12,12 +12,20 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+    context_object_name = 'all_posts_list'
 
 class BlogDetailView(DetailView): # new
     model = Post
     template_name = 'post_detail.html'
+    context_object_name = 'indiv_post'
 
 class BlogCreateView(CreateView): # new
     model = Post
     template_name = 'post_new.html'
     fields = '__all__'
+
+class BlogUpdateView(UpdateView): # new
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
