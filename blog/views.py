18170978679e0e964d71b55ff525131863dac626
LiftEatCode <joshuaspradlingtx@gmail.com>
2020-03-21 21:38:52 -0500

    finished CRUD functionality

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,32 @@
-# blog/views.py
-from django.shortcuts import render
-
-from django.views.generic import ListView, DetailView # new
-
-from django.views.generic.edit import CreateView, UpdateView # new
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Post
 
-# Create your views here.
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
-    context_object_name = 'all_posts_list'
 
-class BlogDetailView(DetailView): # new
+
+class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
-    context_object_name = 'indiv_post'
 
-class BlogCreateView(CreateView): # new
+
+class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
-    fields = '__all__'
+    fields = ['title', 'author', 'body']
 
-class BlogUpdateView(UpdateView): # new
+
+class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
 
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
